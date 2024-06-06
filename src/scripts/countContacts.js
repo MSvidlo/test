4b57840a682bb5f9cb5d825c1939e4716556f0aa
MSvidlo <143841475+MSvidlo@users.noreply.github.com>
2024-06-06 17:02:14 +0200

    Initial commit

---
+++ src/scripts/countContacts.js
@@ -0,0 +1,17 @@
+import { PATH_DB } from '../constants/contacts.js';
+import fs from "node:fs/promises";
+export const countContacts = async () => {
+    try {
+        const allContactsJson = await fs.readFile(PATH_DB, 'utf-8');
+        const  allContacts = JSON.parse(allContactsJson);
+        const caunt = allContacts.length;
+        return caunt
+
+} catch (error) {
+console.error(error.message);
+        return 0;
+}
+
+};
+
+console.log(await countContacts());
